40c32fe18e5582f9ad2e1018c563973e5d6bbfd3
Kale Abelgas <79468220+kaleabelgas@users.noreply.github.com>
2021-04-18 12:54:45 +0800

    create random spawner

--- Bullet Hell Jam Submission/Assets/GameManager.cs
+++ Bullet Hell Jam Submission/Assets/GameManager.cs
@@ -6,7 +6,8 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private List<Transform> spawnPoints;
     [SerializeField] private List<GameObject> enemies;
-    [SerializeField] private int chanceToSpawnEnemy = 1;
+    [SerializeField] private float chanceToSpawnEnemy = 1;
+    [SerializeField] private float chanceToPickSpawnPoint = 1;
 
     private float timePerLevel = 10;
     private float timeRemaining;
@@ -33,24 +34,48 @@ public class GameManager : MonoBehaviour
             timeRemaining = timePerLevel;
             Level++;
             Debug.Log("Current Level: " + Level);
-            //do trigget stuff
+            // TODO: Next Level Event Trigger
+            // TODO: Subscribe IEnumerator SpawnEnemies
         }
     }
 
-    List<GameObject> ChooseEnemies(List<GameObject> enemiesToChooseFrom)
+    List<GameObject> EnemiesChosen(List<GameObject> enemiesToChooseFrom)
     {
         List<GameObject> chosenEnemies = new List<GameObject>();
         
+        chosenEnemies.Add(enemiesToChooseFrom[0]);
+
         for(int i = 0; i < enemiesToChooseFrom.Count; i++)
         {
-            chosenEnemies.Add(enemiesToChooseFrom[0]);
             // RNG
-            int randomNumber = Random.Range(1, 101);
+            float randomNumber = Random.Range(1f, 100f);
             if (randomNumber < chanceToSpawnEnemy)
                 chosenEnemies.Add(enemiesToChooseFrom[i]);
         }
         return chosenEnemies;
     }
 
+    List<Transform> SpawnPointsChosen(List<Transform> spawnPointsToChooseFrom)
+    {
+        // spawn in at least 1
+        List<Transform> chosenSpawnPoints = new List<Transform>();
 
+        chosenSpawnPoints.Add(spawnPointsToChooseFrom[Random.Range(1, spawnPointsToChooseFrom.Count)]);
+
+        for(int i = 0; i < spawnPointsToChooseFrom.Count; i++)
+        {
+            float randomNumber = Random.Range(1f, 100f);
+            if (randomNumber < chanceToSpawnEnemy)
+                chosenSpawnPoints.Add(spawnPointsToChooseFrom[i]);
+        }
+        return chosenSpawnPoints;
+    }
+
+    private void SpawnEnemies()
+    {
+        for(int i = 0; i < SpawnPointsChosen(spawnPoints).Count; i++)
+        {
+            GameObject enemyy = Instantiate(EnemiesChosen(enemies)[i], SpawnPointsChosen(spawnPoints)[i].position, Quaternion.identity);
+        }
+    }
 }
