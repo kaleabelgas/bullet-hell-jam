930a97adf52f77262bafb357e49ccb560affd156
Kale Abelgas <79468220+kaleabelgas@users.noreply.github.com>
2021-05-02 13:48:24 +0800

    create UI tab system

---
+++ Bullet Hell Jam Submission/Assets/ButtonTab.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonTab : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
+{
+    [SerializeField] TabGroup TabGroup;
+    public Image background;
+
+    private void Start()
+    {
+        background = GetComponent<Image>();
+        TabGroup.Subscribe(this);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        TabGroup.OnTabSelected(this);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        TabGroup.OnTabEnter(this);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        TabGroup.OnTabExit(this);
+    }
+
+}
---
+++ Bullet Hell Jam Submission/Assets/TabGroup.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabGroup : MonoBehaviour
+{
+    [SerializeField] private List<ButtonTab> buttonTabs;
+    [SerializeField] private Sprite tabidle;
+    [SerializeField] private Sprite tabHover;
+    [SerializeField] private Sprite tabActive;
+    [SerializeField] private ButtonTab selectedTab;
+
+    [SerializeField] private List<GameObject> objectsToSwap;
+
+    private void Start()
+    {
+        OnTabSelected(selectedTab);
+    }
+
+    public void Subscribe(ButtonTab button)
+    {
+        if (buttonTabs.Equals(null))
+            buttonTabs = new List<ButtonTab>();
+
+        buttonTabs.Add(button);
+    }
+
+    public void OnTabEnter(ButtonTab button)
+    {
+        ResetTabs();
+        if(selectedTab == null || button != selectedTab)
+        {
+            button.background.sprite = tabHover;
+        }
+    }
+
+    public void OnTabExit(ButtonTab button)
+    {
+        ResetTabs();
+    }
+
+    public void OnTabSelected(ButtonTab button)
+    {
+        selectedTab = button;
+        ResetTabs();
+        button.background.sprite = tabActive;
+        int index = button.transform.GetSiblingIndex();
+        for (int i = 0; i < objectsToSwap.Count; i++)
+        {
+            if (i == index)
+            {
+                objectsToSwap[i].SetActive(true);
+            }
+            else
+                objectsToSwap[i].SetActive(false);
+        }
+    }
+
+    public void ResetTabs()
+    {
+        foreach(ButtonTab button in buttonTabs)
+        {
+            if (selectedTab != null && button == selectedTab) { continue; }
+            button.background.sprite = tabidle;
+        }
+    }
+}
